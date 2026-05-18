bb11df5c0f6b3d45561f126e5530d2820fc86fb0
leehyeon2021 <leehyeon2021@gmail.com>
2026-05-18 15:43:20 +0900

    260518

--- day02/T2-01.py
+++ day02/T2-01.py
@@ -1,86 +1,81 @@
 
-# kaggle의 데이터셋 (day01의 Fish.csv 사용)
+# 1차원 -> 2차원 변환
+# T1-01( zip활용 ), T1-02( column_stack활용 ), T2-01( reshape활용 )
 
-# 1. CSV 불러오기
+
+# 1. 가져오기
 import pandas as pd
-df = pd.read_csv('./day02/fish.csv')
-#df.info()
+df = pd.read_csv('./day01/Fish.csv')
+df.info()
 
-# 2. 필요한 어종 추출: 논리식 대신에 `.isin( )` 특정값만 추출, `isna( )` 결측치만 추출
-target_fish = df[ df['Species'].isin( [ 'Bream', 'Smelt'] ) ]
-#print( target_fish.head() )
+# 2. Perch(농어)만 추출
+target_fish = df[ df['Species'].isin(['Perch'])]
+target_fish.info()  # 56마리
+# 농어의 길이/무게 추출
+perch_length = target_fish['Length2'].values
+perch_weight = target_fish['Weight'].values
 
-# 3. 필요한 특성 추출: 2차원리스트 -> [[Length2, Weight],[Length2, Weight]]
-# T1-01의 `.zip()` 대신, 넘파이로 `np.column_stack()`
-# `np.column_stack( (리스트1), (리스트2) )`: 두 리스트 간의 동일한 요소로 2차원 리스트 구성
-import numpy as np
-fish_data = np.column_stack( ( target_fish['Length2'], target_fish['Weight'] ) )
-print(fish_data)
-
-# 4. 모델 학습을 하기 위한 정답지: 도미 35마리, 빙어 14마리
-fish_target = np.concatenate( (np.ones(35) , np.zeros(14)) )
-print(fish_target)
-
-# 5. 학습 모델 만들기 전 학습용, 테트스용 분리
-from sklearn.model_selection import train_test_split
-# 학습용자료, 테스트용자료, 학습용정답지, 테스트용정답지 = train_test_split( 학습자료, 정답지, test_size=테스트자료비율 )
-# 4개의 반환 타입을 갖는다.
-train_input , test_input, train_target, test_target = train_test_split(fish_data , fish_target, test_size=0.3) #학습용7:테스트
-
-# 6. 학습모델: k-최근접 이웃 분류기 모델
-from sklearn.neighbors import KNeighborsClassifier
-kn = KNeighborsClassifier()                     # 모델 객체 생성 (new없음확인)
-kn.fit( train_input , train_target )            # 모델 (지도) 학습
-print( kn.score( test_input , test_target ) )   # 모델 평가 (1:100%)
-
-# 7. 임의의 값으로 학습 무게 예측하기
-# 길이: 25cm , 무게: 150g의 물고기가 도미[1]인지, 빙어[0]인지 예측하기
-print( kn.predict( [ [25, 150]]) )  # 모델 예측. 0[빙어] ---> 잘못된 예측
-
-
-# 8. 예측값 시각화
+# 농어 '길이'에 따른 '무게' 예측
 import matplotlib.pyplot as plt
-# train_input[ : , 0 ] : [ 행슬라이싱 , 열슬라이싱 ] : 모든 행의 0번째 열만 추출 -> 길이만 추출
-# train_input[ : , 1 ] : [ 행슬라이싱 , 열슬라이싱 ] : 모든 행의 1번째 열만 추출 -> 무게만 추출
-plt.scatter( train_input[ : , 0 ] , train_input[ : , 1 ] )  # 학습용
-plt.scatter( 25, 150 )  # 예측값
+plt.scatter( perch_length , perch_weight )
 plt.show()
 
-# 9. 예측하기 위한 이웃들 확인: `.kneighbors([ 예측값 ])` 예측에 사용된 이웃(거리, 인덱스)들을 반환
-dist , index = kn.kneighbors( [ [25, 150] ] )
-plt.scatter( train_input[ : , 0], train_input[:, 1])                # 학습용
-plt.scatter( train_input[:,0] , train_input[:,1] )                  # 예측값
-plt.scatter( train_input[ index , 0] , train_input[ index , 1] )    # 예측에 사용된 이웃 자료 -> 문제 발견
-plt.show()
+# 3. 학습 모델 만들기
+# 1) 준비: 학흡용과 테스트용 분리. -왜?-> 모델평가에 사용하려고.
+from sklearn.model_selection import train_test_split
+train_input, test_input, train_target, test_target = train_test_split( perch_length, perch_weight, test_size=0.3, random_state=42 )
+    # `random_state=분리할 때 사용되는 난수값`: 난수값에 따라 분리한다. 0~32억 사이 아무 값이나 넣어주면 된다.
+    # 고정값을 넣어주면 항상 동일한 분리 값을 넣을 수 있다.
+# 2) 자료형식(모양) 구성: (사이킷런)모델 학습 전에 형식 만들기. 대부분 2차원 구성
+import numpy as np
+# `shape`: 배열의 모양을 반환해준다.
+array = np.array( [1, 2, 3, 4] )    # 1차원: (4,) <-(행 , 열)
+print( array.shape )
+array2 = np.array([[1, 2],[3, 4],[5, 6]])   # 2차원: (3, 2)
+print( array2.shape )
+print( train_input.shape )  # (39,) 1차원 배열 나옴 -> 사이킷런 모델들은 1차원배열 학습이 불가능하다.
+print( train_input )        # 1차원으로 구성된 '농어' 길이
 
-# 문제 발생 => 잘못된 예측값(누가봐도 빙어 아닌데 빙어라고 그럼)
+# 4. 1차원 -> 2차원
+# T1-01( zip활용 ), T1-02( column_stack활용 ), T2-01( reshape ) 1차원->2차원
+# `.reshape( 행개수, 열개수 )`: 행개수에는 -1 넣음 (-1은 자동으로 해달라는 뜻(자료개수만자동)), 열 개수는 1개
+train_input = train_input.reshape( -1, 1 )  # 2차원으로 바뀜!
+print( train_input )  # [17.4 36.  25.  40. (생략)] -> [[17.4] [36. ] [25. ] [40. ] (생략) ]
+test_input = test_input.reshape( -1, 1 )
 
-# 10. 스케일, 표준화 필요성: [공정하게 크기단위를 맞추는 작업] 길이와 무게 값의 차이가 커서 일관된 비교가 어렵다.
-# 예) 달리기 점수 80 90 70 , 몸무게 40 45 100 , 달리기 70 ~ 80 , 몸무게 40 ~ 100
-# => 컴퓨터는 숫자가 더 큰 걸 무엇보다 중요하게 생각한다. 😮
-# 몸무게 40 = -1 , 몸무게 100 - 1 , 몸무게 50 = 0 취급하여 비교한다.
-# 즉) 특정한 자료의 단위 크기가 크면 큰 값이 모델을 지배하지 않도록 특정기준으로 맞춰주어야 한다.
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()   # 스케일 객체 생성
-scaler.fit( train_input )   # [0.]
-print( scaler.mean_ )       # 평균: [ 25.86764706 415.78823529]
-print( scaler.scale_)       # 표준편차: [ 10.45833098 322.40799875]
-train_scaled = scaler.transform( train_input )  # 표준화( 스케일링 )
-print( train_scaled )
+# 5. 모델 학습
+from sklearn.neighbors import KNeighborsClassifier  # K최근접이웃 모델
+from sklearn.neighbors import KNeighborsRegressor   # K최근접이웃 회귀
+knr = KNeighborsRegressor()                         # 모델 객체 생성
+knr.fit( train_input , train_target )               # 모델 학습 (길이, 무게) <- '길이'에 따른 '무게' 학습
+print( knr.score(test_input, test_target))          # 모델 평가
+print( test_input )
+print( knr.predict( test_input ) )
 
-# 11. 스케일링 이후 시작화
-plt.scatter( train_scaled[ : , 0], train_scaled[ : , 1])
-plt.show()  # 차트 모양의 차이는 없지만 단위가 표준화 되었다.
+# 6. k최근접이웃 회귀는 이웃의 평균으로 예측 한다. 하이퍼라미터(k) 조절
+# k = 이웃 개수 정하기
+knr = KNeighborsRegressor()                         # 모델 객체 생성
+# 임의의 길이 생성: 임의의 물고기 길이 5부터 45까지 생성 (45개 임의값)
+x = np.arange( 5 , 45 ).reshape( -1 , 1 )
+print( x )  # 5 ~ 44 까지의 임의의 값
+for k in [ 1 , 3 , 5 , 10 ]:                        # 이웃 개수를 4가지 모델로 학습
+    knr.n_neighbors = k                             # 현재 모델의 이웃개수 대입
+    knr.fit( train_input , train_target )           # 총 4번 학습예정
+    print( knr.score(test_input , test_target) )    # 총 4번 학습평가
+    pred = knr.predict( x )                         # 임의의 값 x(물고기 길이)로 예측
+    print( pred )                                   # 총 45개의 물고기 길이의 몸무게를 예측한다.
+    # 시각화
+    plt.scatter( train_input , train_target )
+    plt.plot( x , pred )                            # plot ( 선차트이면서 회귀(예측)선 ) x=길이 pred=몸무게(예측)
+    plt.show()
 
-# 12. 스케일링 이후 재학습 모델 만들기
-kn.fit( train_scaled , train_target )   # 표준화 된 자료로 재학습
-# 임의의 예측값( 스케일링된 )
-new = scaler.transform( [ [25, 150] ] ) # 스케일 하고 나서 하면 # 150 값 조절해서 보기
-# 예측하기
-print( kn.predict( new ))               # [1.] 1이 나옴!
-# 예측에 사용된 이웃들 확인
-dist , indexs = kn.kneighbors( new )
-plt.scatter( train_scaled[:,0], train_scaled[:,1])              # 스케일링된 학습용
-plt.scatter( new[ : , 0] , new[ : , 1 ] )     # 스케일링된 예츠값
-plt.scatter( train_scaled[indexs, 0] , train_scaled[ indexs, 1] )  # 예측에 사용된 이웃 자료
-plt.show()
+# k는 이웃개수 뜻한다. k최근접 회귀는 이웃의 평균으로 예측한다.
+# k가 1일 때   0.9918926744767643       -> 과대 적합 훈련: 특정한 자료에 튀는 데이터(노이즈/이상치)까지 적용될 수 있으므로 예측이 망가질 수 있다.
+# k가 3일 때   0.9766857219041255
+# k가 5일 때   0.9929281790592219   -> 균형적인 추세 표현
+# k가 10일 때  0.9742254836937329       -> 과소 적합 훈련: 많은 자료에 둔감한 단순화된 자료까지 적용될 수 있으므로 예측이 망가질 수 있다.
+# k가 5일 때 가장 균형적인 추세 표현이다. 회귀선이 너무 꺾이거나 완만한 일직선이 아니다.
+    # 그래프가 확 꺾여 뾰족한 것 보다는 완만한 것이 좋대
+        # 1일 때는 너무 직선이 너무 많아 뾰족해서 x , 10일 때는 직선이 너무 많아 일직선이라서 x
+
+# 결론: 머신러닝에서는 가장 최적의 파라미터(k/이웃개수)를 찾는 과정을 한다. 이를 '튜닝'이라 한다.
