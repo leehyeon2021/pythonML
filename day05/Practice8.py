3f4acb66e32dadd18d22664b6004784bd2d9bcd8
leehyeon2021 <leehyeon2021@gmail.com>
2026-05-21 18:22:10 +0900

    260521

--- day05/Practice8.py
+++ day05/Practice8.py
@@ -9,8 +9,6 @@ df = pd.read_csv( './day04/student_dataset_10000_rows.csv')
 # 타깃을 제외한 study_hours, attendance 등 총 6개의 수치형 특성을 독립변수로 지정하고, 
 # 전체 데이터를 학습 세트와 검증 세트 비율 8:2로 정확하게 분리하시오.
 
-placement_status = df['Placed', 'Not Placed']
-
 
 # [2] 특성 다항 확장 및 경사하강법 필수 스케일링
 
